e0d89fa342b5ee639cbe145ccd25382cc980b1e4
MatamaPeter <peter24matama@gmail.com>
2025-01-01 17:33:52 +0300

    connected a json file using routes

---
+++ routes/api/employees.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+const data = {};
+
+data.employees = require('../../data/employees.json');
+
+router.route('/')
+    .get((req, res) => {
+        res.json(data.employees)
+    })
+    .post((req, res) => {
+        res.json({
+            'name': req.body.name,
+            'profession': req.body.profession
+        });
+    })
+    .put((req, res) => {
+         res.json({
+            'name': req.body.name,
+            'profession': req.body.profession
+        });
+    })
+    .delete((req, res) => {
+        res.json({
+            'id': req.body.id
+        })
+    })
+
+router.route('/:id')
+    .get((req, res) => {
+        res.json({
+            "id":req.params.id
+        })
+    })
+
+module.exports = router;
