1fa2419061b19dea2953df85289736ec734049f6
MatamaPeter <peter24matama@gmail.com>
2025-01-04 09:50:20 +0300

    JWT authorization

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -42,7 +42,7 @@ const handleLogin = async (req, res) => {
             path.join(__dirname, '..', 'model', 'users.json'),
             JSON.stringify(userDB.users)
         );
-        res.cookie('jwt', refreshToken, { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 });
+        res.cookie('jwt', refreshToken, { httpOnly: true, sameSite:'None',maxAge: 24 * 60 * 60 * 1000 });
         res.json({ accessToken });
 
     } else {
